4a7a5bc075c66df67d180c8e0bfbc41bc3d9650d
rishabgaddi <rishab03@live.com>
2022-01-09 17:24:09 +0530

    Added delete icon and adjusted flex

--- App.js
+++ App.js
@@ -59,9 +59,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff'
   },
   content: {
-    padding: 40
+    padding: 40,
+    flex: 1
   },
   list: {
-    margin: 10
+    margin: 10,
+    flex: 1
   }
 });
--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,9 +1,13 @@
-import { StyleSheet, Text, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function TodoItem({item, pressHandler}) {
   return (
     <TouchableOpacity onPress={() => pressHandler(item.key)}>
-      <Text style={styles.item}>{item.text}</Text>
+      <View style={styles.item}>
+        <MaterialIcons name='delete' size={18} color='#333' />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   )
 }
@@ -15,6 +19,10 @@ const styles = StyleSheet.create({
     borderColor: '#bbb',
     borderWidth: 1,
     borderStyle: 'dashed',
-    borderRadius: 10
+    borderRadius: 10,
+    flexDirection: 'row'
+  },
+  itemText: {
+    marginLeft: 10
   }
 })
