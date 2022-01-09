af290715eb24b51f85a60ad8e6b1fb45e26f5b45
rishabgaddi <rishab03@live.com>
2022-01-09 16:29:39 +0530

    Add todo feature

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { useState } from 'react';
 import Header from './components/Header';
 import TodoItem from './components/TodoItem';
+import AddTodo from './components/AddTodo';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -17,11 +18,19 @@ export default function App() {
     })
   }
 
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        {text: text, key: Math.random().toString()},
+        ...prevTodos
+      ]
+    })
+  }
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/*Todo content*/}
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
             data={todos}
